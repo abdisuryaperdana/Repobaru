fd5b77810180c8f8dc666846e803a5342b499b18
Abdi Surya Perdana <abdisury4@gmail.com>
2022-10-18 12:42:08 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Portofolio/',
+  base:'/Repobaru/',
   plugins: [vue()],
   resolve: {
     alias: {
